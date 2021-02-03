c376f4bb7ae9ba8bf828f5fc07f2bc2c804df23b
MrTroble <nifroh56@gmail.com>
2021-02-03 20:34:55 +0100

    Added progress bar

---
+++ src/main/java/com/troblecodings/launcher/node/ProgressBar.java
@@ -0,0 +1,26 @@
+package com.troblecodings.launcher.node;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class ProgressBar extends Node {
+
+	private final int max;
+	private final Color color;
+	
+	public ProgressBar(int x1, int y1, int width, int height, Color color) {
+		super(x1, y1, 0, height);
+		max = width;
+		this.color = color;
+	}
+
+	public void update(float percentage) {
+		this.x2 = (int) (percentage * max);
+	}
+	
+	@Override
+	public void draw(Graphics gr) {
+		gr.setColor(this.color);
+		gr.fillRect(x1, y1, x2, y2);
+	}
+}
