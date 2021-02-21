6f54f5a24a700480cf6968b60a1700efb85912d1
Der-Zauberer <and.som282@gmail.com>
2021-02-21 11:05:12 +0100

    Added combobox for options

--- src/main/java/com/troblecodings/launcher/javafx/OptionsScene.java
+++ src/main/java/com/troblecodings/launcher/javafx/OptionsScene.java
@@ -2,13 +2,14 @@ package com.troblecodings.launcher.javafx;
 
 import com.troblecodings.launcher.Launcher;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 
 public class OptionsScene extends Scene {
 	
 	private static StackPane stackpane = new StackPane();
-
 	public OptionsScene() {
 		super(stackpane);
 		Launcher.setupScene(this, stackpane);
@@ -17,6 +18,17 @@ public class OptionsScene extends Scene {
 		vbox.setMaxHeight(400);
 		vbox.setMaxWidth(500);
 		stackpane.getChildren().add(vbox);
+		
+		Label ramlabel = new Label("RAM");
+		ramlabel.setStyle("-fx-padding: 0px 0px 10px 0px;");
+
+		ComboBox<String> ramcombobox = new ComboBox<String>();
+		ramcombobox.getItems().addAll("1 GB", "2 GB", "4GB", "6 GB", "8GB", "10 GB", "16 GB", "20 GB", "24 GB");
+		ramcombobox.setPromptText("1 GB");
+		
+		vbox.getChildren().addAll(ramlabel, ramcombobox);
+		
+		
 	}
 
 }
