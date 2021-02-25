6a22e8db42b953ac5cfcb0ab85301beb2e5bed99
MrTroble <nifroh56@gmail.com>
2021-02-25 17:48:51 +0100

    Fixed rolling log files

--- src/main/java/com/troblecodings/launcher/javafx/Footer.java
+++ src/main/java/com/troblecodings/launcher/javafx/Footer.java
@@ -4,18 +4,25 @@ import com.troblecodings.launcher.Launcher;
 
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.layout.StackPane;
 
 public class Footer extends StackPane {
 	
+	private static ProgressBar progressBar = null;
+	
+	
 	public Footer() {
+		if(progressBar == null)
+			progressBar = new ProgressBar();
+		
 		Button button = new Button("Lizensen und Kredits");
 		button.setOnAction(event -> onButtonClicked());
-		button.setTranslateY(20);
 		
-		this.getChildren().add(button);
+		this.getChildren().addAll(button, progressBar);
 		this.setMaxHeight(85);
-		StackPane.setAlignment(this, Pos.BOTTOM_CENTER);
+		StackPane.setAlignment(this, Pos.BOTTOM_LEFT);
+		StackPane.setAlignment(button, Pos.BOTTOM_CENTER);
 	}
 	
 	private void onButtonClicked() {
