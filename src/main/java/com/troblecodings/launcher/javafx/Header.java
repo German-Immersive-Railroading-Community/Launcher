6091be9f0b53d6374b93cf707eaad50307c89305
MrTroble <nifroh56@gmail.com>
2021-03-01 14:48:07 +0100

    Added new header

--- src/main/java/com/troblecodings/launcher/javafx/Header.java
+++ src/main/java/com/troblecodings/launcher/javafx/Header.java
@@ -1,27 +1,45 @@
 package com.troblecodings.launcher.javafx;
 
+import java.util.function.Supplier;
+
 import com.troblecodings.launcher.Launcher;
+
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 
 public class Header extends StackPane {
-	
-	 private static double xOffset = 0;
-	 private static double yOffset = 0;
-	
-	public Header(Scene scene) {
-		Button optionsbutton = new Button();
-		if(scene instanceof HomeScene) {
-			optionsbutton.getStyleClass().add("optionsbutton");
-			optionsbutton.setOnAction(event -> Launcher.setScene(Launcher.OPTIONSSCENE));
-		} else {
- 			optionsbutton.getStyleClass().add("backbutton");
-			optionsbutton.setOnAction(event -> Launcher.setScene(Launcher.HOMESCENE));
+
+	private static double xOffset = 0;
+	private static double yOffset = 0;
+
+	private static enum EnumPages {
+		HOME(() -> Launcher.HOMESCENE),
+		OPTIONS(() -> Launcher.OPTIONSSCENE);
+
+		public final Supplier<Scene> supplier;
+
+		private EnumPages(Supplier<Scene> run) {
+			this.supplier = run;
 		}
+	}
+
+	public Header(Scene scene) {
+		HBox hbox = new HBox();
+		for(EnumPages page : EnumPages.values()) {
+			Button btn = new Button();
+			btn.getStyleClass().add("navbar");
+			btn.setText(page.name().toUpperCase());
+			btn.setOnAction(evt -> Launcher.setScene(page.supplier.get()));
+			hbox.getChildren().add(btn);
+		    HBox.setMargin(btn, new Insets(0, 20, 0, 20));
+		}
+		hbox.setAlignment(Pos.CENTER);
 		
 		Button closebutton = new Button();
 		closebutton.getStyleClass().add("closebutton");
@@ -29,28 +47,28 @@ public class Header extends StackPane {
 		closebutton.setTranslateX(-20);
 		closebutton.setTranslateY(20);
 		StackPane.setAlignment(closebutton, Pos.TOP_RIGHT);
-		
-		this.getChildren().addAll(optionsbutton, closebutton);
+
+		this.getChildren().addAll(hbox, closebutton);
 		this.setMaxHeight(85);
 		StackPane.setAlignment(this, Pos.TOP_CENTER);
 		initializeEvents();
 	}
-	
+
 	private void initializeEvents() {
 		this.setOnMousePressed(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                xOffset = event.getSceneX();
-                yOffset = event.getSceneY();
-            }
-        });
-        this.setOnMouseDragged(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                Launcher.getStage().setX(event.getScreenX() - xOffset);
-                Launcher.getStage().setY(event.getScreenY() - yOffset);
-            }
-        });
+			@Override
+			public void handle(MouseEvent event) {
+				xOffset = event.getSceneX();
+				yOffset = event.getSceneY();
+			}
+		});
+		this.setOnMouseDragged(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				Launcher.getStage().setX(event.getScreenX() - xOffset);
+				Launcher.getStage().setY(event.getScreenY() - yOffset);
+			}
+		});
 	}
 
 }
