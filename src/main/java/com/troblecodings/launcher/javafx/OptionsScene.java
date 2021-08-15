5d00cfa53de72330b9f5cc32e2001dcf82ac5aef
MrTroble <nifroh56@gmail.com>
2021-08-15 19:07:36 +0200

    Fixed issue with base dir

--- src/main/java/com/troblecodings/launcher/javafx/OptionsScene.java
+++ src/main/java/com/troblecodings/launcher/javafx/OptionsScene.java
@@ -122,7 +122,7 @@ public class OptionsScene extends Scene {
 		baseDir.setStyle("-fx-padding: 20px 0px 10px 0px;");
 
 		final TextField baseDirField = new TextField(FileUtil.SETTINGS.baseDir);
-		baseDirField.textProperty().addListener((obs, old, ne) -> FileUtil.moveBaseDir(baseDirField.getText()));
+		baseDirField.setEditable(false);
 
 		final Button baseDirFinder = new Button("Open Folder");
 		baseDirFinder.getStyleClass().add("optionButton");
@@ -132,6 +132,7 @@ public class OptionsScene extends Scene {
 			final File fl = chooser.showDialog(Launcher.getStage());
 			if (fl != null && fl.exists() && fl.isDirectory()) {
 				baseDirField.setText(fl.toString());
+				FileUtil.moveBaseDir(baseDirField.getText());
 			}
 		});
 
