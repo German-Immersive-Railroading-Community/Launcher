b47218bd0fedc072006d81d0da5eb09e5d469366
Shirosaka <shiro@shirosaka.dev>
2023-09-13 20:31:10 +0200

    feat(auth)!: update microsoft login scene and delete mojang login

--- src/main/java/eu/girc/launcher/javafx/LoginScene.java
+++ src/main/java/eu/girc/launcher/javafx/LoginScene.java
@@ -1,7 +1,6 @@
 package eu.girc.launcher.javafx;
 
 import eu.girc.launcher.Launcher;
-
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -12,37 +11,30 @@ import javafx.scene.layout.VBox;
 
 public class LoginScene extends Scene {
 
-	private static StackPane stackpane = new StackPane();
+    private static final StackPane STACK_PANE = new StackPane();
 
-	public LoginScene() {
-		super(stackpane);
-		Launcher.setupScene(this, stackpane);
+    public LoginScene() {
+        super(STACK_PANE);
+        Launcher.setupScene(this, STACK_PANE);
 
-		VBox vbox = new VBox();
-		vbox.setMaxHeight(400);
-		vbox.setMaxWidth(625);
-		vbox.setAlignment(Pos.CENTER);
-		stackpane.getChildren().add(vbox);
+        VBox vbox = new VBox();
+        vbox.setMaxHeight(400);
+        vbox.setMaxWidth(625);
+        vbox.setAlignment(Pos.CENTER);
+        STACK_PANE.getChildren().add(vbox);
 
-		final Button mojangLoginButton = new Button();
-		mojangLoginButton.getStyleClass().add("mojangLoginButton");
-		VBox.setMargin(mojangLoginButton, new Insets(20, 0, 20, 0));
-		mojangLoginButton.setOnAction(handler -> {
-			Launcher.setScene(Launcher.MOJANGLOGINSCENE);
-		});
+        final Button microsoftLoginButton = new Button();
+        microsoftLoginButton.getStyleClass().add("microsoftLoginButton");
+        VBox.setMargin(microsoftLoginButton, new Insets(20, 0, 20, 0));
+        microsoftLoginButton.setOnAction(event -> {
+            Launcher.setScene(Launcher.MICROSOFTLOGINSCENE);
+        });
 
-		final Button microsoftLoginButton = new Button();
-		microsoftLoginButton.getStyleClass().add("microsoftLoginButton");
-		VBox.setMargin(microsoftLoginButton, new Insets(20, 0, 20, 0));
-		microsoftLoginButton.setOnAction(event -> {
-			Launcher.setScene(Launcher.MICROSOFTLOGINSCENE);
-		});
+        vbox.getChildren().add(microsoftLoginButton);
 
-		vbox.getChildren().addAll(mojangLoginButton, microsoftLoginButton);
-		
-		final ImageView trainImageView = new ImageView(Launcher.getImage("train2.png"));
-		trainImageView.setTranslateX(760 - trainImageView.getImage().getWidth());
-		trainImageView.setTranslateY(325 - trainImageView.getImage().getHeight());
-		stackpane.getChildren().add(trainImageView);
-	}
+        final ImageView trainImageView = new ImageView(Launcher.getImage("train2.png"));
+        trainImageView.setTranslateX(760 - trainImageView.getImage().getWidth());
+        trainImageView.setTranslateY(325 - trainImageView.getImage().getHeight());
+        STACK_PANE.getChildren().add(trainImageView);
+    }
 }
--- src/main/java/eu/girc/launcher/javafx/MicrosoftLoginScene.java
+++ src/main/java/eu/girc/launcher/javafx/MicrosoftLoginScene.java
@@ -1,12 +1,7 @@
 package eu.girc.launcher.javafx;
 
-import java.net.CookieHandler;
-import java.net.CookieManager;
-import java.net.CookiePolicy;
-
 import eu.girc.launcher.Launcher;
 import eu.girc.launcher.util.AuthUtil;
-
 import javafx.application.Platform;
 import javafx.collections.ListChangeListener;
 import javafx.geometry.Pos;
@@ -20,64 +15,68 @@ import javafx.scene.web.WebView;
 import net.hycrafthd.minecraft_authenticator.login.AuthenticationException;
 import net.hycrafthd.minecraft_authenticator.login.Authenticator;
 
+import java.net.CookieHandler;
+import java.net.CookieManager;
+import java.net.CookiePolicy;
+
 public class MicrosoftLoginScene extends Scene {
-	
-	private static StackPane stackpane = new StackPane();
-	
-	private static WebEngine engine; 
-	
-	public MicrosoftLoginScene() {
-		super(stackpane);
-		Launcher.setupScene(this, stackpane);
-		
-		VBox vbox = new VBox();
-		vbox.setMaxHeight(500);
-		vbox.setMaxWidth(625);
-		vbox.setAlignment(Pos.CENTER);
-		stackpane.getChildren().add(vbox);
-		
-		// Setup cookie manager
-		final CookieManager manager = new CookieManager();
-		manager.setCookiePolicy(CookiePolicy.ACCEPT_ALL);
-		CookieHandler.setDefault(manager);
-		
-		final WebView webView = new WebView();
-		webView.setMinWidth(600);
-		engine = webView.getEngine();
-		
-		engine.setJavaScriptEnabled(true);
-		engine.load(Authenticator.microsoftLogin().toString());
-		
-		webView.getEngine().getHistory().getEntries().addListener(this::loginCheck);
-		
-		vbox.getChildren().addAll(webView);
-		
-		final ImageView trainImageView = new ImageView(Launcher.getImage("train2.png"));
-		trainImageView.setTranslateX(760 - trainImageView.getImage().getWidth());
-		trainImageView.setTranslateY(325 - trainImageView.getImage().getHeight());
-		stackpane.getChildren().add(trainImageView);
-	}
-	
-	private void loginCheck(ListChangeListener.Change<? extends WebHistory.Entry> event) {		
-		if (event.next() && event.wasAdded()) {
-			for (WebHistory.Entry entry : event.getAddedSubList()) {
-				if (entry.getUrl().startsWith(Authenticator.microsoftLoginRedirect())) {
-					final String authCode = entry.getUrl().substring(entry.getUrl().indexOf("=") + 1, entry.getUrl().indexOf("&"));
-					
-					try {
-						AuthUtil.microsoftLogin(authCode);
-						Platform.runLater(() -> {
-							Header.setVisibility(true);
-							Launcher.setScene(Launcher.HOMESCENE);
-						});
-					} catch (AuthenticationException ex) {
-						// Platform.runLater(() -> error.setText("Wrong credentials!"));
-						engine.reload();
-						Launcher.onError(ex);
-					}
-				}
-			}
-		}
-	}
-	
+
+    private static final StackPane stackpane = new StackPane();
+
+    private static WebEngine engine;
+
+    public MicrosoftLoginScene() {
+        super(stackpane);
+        Launcher.setupScene(this, stackpane);
+
+        VBox vbox = new VBox();
+        vbox.setMaxHeight(500);
+        vbox.setMaxWidth(625);
+        vbox.setAlignment(Pos.CENTER);
+        stackpane.getChildren().add(vbox);
+
+        // Setup cookie manager
+        final CookieManager manager = new CookieManager();
+        manager.setCookiePolicy(CookiePolicy.ACCEPT_ORIGINAL_SERVER);
+        CookieHandler.setDefault(manager);
+
+        final WebView webView = new WebView();
+        webView.setMinWidth(600);
+        engine = webView.getEngine();
+
+        engine.setJavaScriptEnabled(true);
+        engine.load(Authenticator.microsoftLogin().toString());
+
+        webView.getEngine().getHistory().getEntries().addListener(this::loginCheck);
+
+        vbox.getChildren().addAll(webView);
+
+        final ImageView trainImageView = new ImageView(Launcher.getImage("train2.png"));
+        trainImageView.setTranslateX(760 - trainImageView.getImage().getWidth());
+        trainImageView.setTranslateY(325 - trainImageView.getImage().getHeight());
+        stackpane.getChildren().add(trainImageView);
+    }
+
+    private void loginCheck(ListChangeListener.Change<? extends WebHistory.Entry> event) {
+        if (event.next() && event.wasAdded()) {
+            for (WebHistory.Entry entry : event.getAddedSubList()) {
+                if (entry.getUrl().startsWith(Authenticator.microsoftLoginRedirect())) {
+                    final String authCode = entry.getUrl().substring(entry.getUrl().indexOf("=") + 1, entry.getUrl().indexOf("&"));
+
+                    try {
+                        AuthUtil.microsoftLogin(authCode);
+                        Platform.runLater(() -> {
+                            Header.setVisibility(true);
+                            Launcher.setScene(Launcher.HOMESCENE);
+                        });
+                    } catch (AuthenticationException ex) {
+//						 Platform.runLater(() -> error.setText("Wrong credentials!"));
+                        engine.reload();
+                        Launcher.onError(ex);
+                    }
+                }
+            }
+        }
+    }
+
 }
