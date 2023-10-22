1ac219e90e75eabe1c74e2967fbc5d965c45de7d
Shirosaka <shiro@shirosaka.dev>
2023-10-22 13:27:00 +0200

    fix: add missing module declarations

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,11 @@
 module eu.girc.launcher {
     requires com.google.gson;
+    requires com.google.common;
     requires javafx.base;
     requires javafx.controls;
     requires javafx.web;
     requires org.apache.logging.log4j;
+    requires org.apache.commons.lang3;
     requires java.base;
     requires java.desktop;
     requires net.hycrafthd.minecraft_authenticator;
