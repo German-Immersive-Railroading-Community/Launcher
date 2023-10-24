89e9c012cee699efc50e9a61b798d56c36f03641
Shirosaka <shiro@shirosaka.dev>
2023-10-24 18:44:14 +0200

    feat: remove the 'beta-mods' functionality from the options scene

--- src/main/java/eu/girc/launcher/Launcher.java
+++ src/main/java/eu/girc/launcher/Launcher.java
@@ -30,7 +30,6 @@ import org.apache.logging.log4j.Logger;
 public class Launcher extends Application {
     private static Logger logger;
     private static Launcher instance = null;
-    private static boolean beta_mode = false;
 
     private static final List<Image> images = new ArrayList<>();
 
@@ -69,10 +68,6 @@ public class Launcher extends Application {
             if ("--no-update".equals(param)) {
                 logger.info("Skipping updates!");
                 update = false;
-            } else if ("-eb".equals(param) || "--enable-beta".equals(param)) {
-                logger.info("Enabling access to beta versions.");
-                beta_mode = true;
-                StartupUtil.refreshBetaJson();
             }
         }
 
@@ -142,10 +137,6 @@ public class Launcher extends Application {
         stackpane.getChildren().add(new Footer(scene));
         scene.setFill(Color.TRANSPARENT);
         scene.getStylesheets().add(getStyleSheet("style.css"));
-    }
-
-    public static boolean getBetaMode() {
-        return beta_mode;
     }
 
     public static Scene getScene() {
