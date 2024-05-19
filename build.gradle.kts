6c539288c325c72fba2f875d73e80377bc8a7a84
Shirosaka <shiro@shirosaka.dev>
2024-05-20 01:24:37 +0200

    chore(deps): update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("java")
     id("application")
-    id("io.sentry.jvm.gradle") version "4.4.0"
+    id("io.sentry.jvm.gradle") version "4.6.0"
     id("com.github.gmazzo.buildconfig") version "5.3.5"
 //    id("org.javamodularity.moduleplugin") version "1.8.12"
     id("org.openjfx.javafxplugin") version "0.1.0"
@@ -66,15 +66,15 @@ repositories {
     }
 }
 
-val junitVersion = "5.10.0"
+val junitVersion = "5.10.2"
 dependencies {
-    implementation("org.apache.logging.log4j:log4j-api:2.17.2")
-    implementation("org.apache.logging.log4j:log4j-core:2.17.2")
-    implementation("org.apache.commons:commons-lang3:3.13.0")
+    implementation("org.apache.logging.log4j:log4j-api:2.23.1")
+    implementation("org.apache.logging.log4j:log4j-core:2.23.1")
+    implementation("org.apache.commons:commons-lang3:3.14.0")
     implementation("org.apache.commons:commons-compress:1.26.1")
     implementation("net.hycrafthd:minecraft_authenticator:3.0.6")
     implementation("com.google.code.gson:gson:2.10.1")
-    implementation("com.google.guava:guava:32.1.2-jre")
+    implementation("com.google.guava:guava:32.2.0-jre")
 
     testImplementation(platform("org.junit:junit-bom:${junitVersion}"))
     testImplementation("org.junit.jupiter:junit-jupiter")
