75b490280844f02fb470e9373ea6768ea83e1c65
Shirosaka <shiro@shirosaka.dev>
2025-09-20 20:33:12 +0200

    chore(formatting): reformat header a bit

--- src/main/java/com/troblecodings/launcher/javafx/Header.java
+++ src/main/java/com/troblecodings/launcher/javafx/Header.java
@@ -17,73 +17,68 @@ import javafx.scene.layout.StackPane;
 
 public class Header extends StackPane {
 
-	private static double xOffset = 0;
-	private static double yOffset = 0;
+    private static double xOffset = 0;
+    private static double yOffset = 0;
 
-	private static enum EnumPages {
-		HOME(() -> Launcher.HOMESCENE), OPTIONS(() -> Launcher.OPTIONSSCENE);
+    private static enum EnumPages {
+        HOME(() -> Launcher.HOMESCENE), OPTIONS(() -> Launcher.OPTIONSSCENE);
 
-		public final Supplier<Scene> supplier;
+        public final Supplier<Scene> supplier;
 
-		private EnumPages(Supplier<Scene> run) {
-			this.supplier = run;
-		}
-	}
+        EnumPages(Supplier<Scene> run) {
+            this.supplier = run;
+        }
+    }
 
-	private static final ArrayList<Button> _buttons = new ArrayList<>();
+    private static final ArrayList<Button> _buttons = new ArrayList<>();
 
-	public Header(Scene scene) {
-		HBox hbox = new HBox();
-		if (!(scene instanceof LoginScene)) {
-			for (EnumPages page : EnumPages.values()) {
-				Button btn = new Button();
-				btn.getStyleClass().add("navbar");
-				btn.setText(page.name().toUpperCase());
-				btn.setOnAction(evt -> {
-					if(!AuthUtil.checkSession()) return;
-					Launcher.setScene(page.supplier.get());
-				});
-				hbox.getChildren().add(btn);
-				HBox.setMargin(btn, new Insets(20, 20, 20, 20));
-				_buttons.add(btn);
-			}
-		}
-		hbox.setAlignment(Pos.CENTER);
+    public Header(Scene scene) {
+        HBox hbox = new HBox();
+        if (!(scene instanceof LoginScene)) {
+            for (EnumPages page : EnumPages.values()) {
+                Button btn = new Button();
+                btn.getStyleClass().add("navbar");
+                btn.setText(page.name().toUpperCase());
+                btn.setOnAction(evt -> {
+                    if (!AuthUtil.checkSession()) return;
+                    Launcher.setScene(page.supplier.get());
+                });
+                hbox.getChildren().add(btn);
+                HBox.setMargin(btn, new Insets(20, 20, 20, 20));
+                _buttons.add(btn);
+            }
+        }
+        hbox.setAlignment(Pos.CENTER);
 
-		Button closebutton = new Button();
-		closebutton.getStyleClass().add("closebutton");
-		closebutton.setOnAction(event -> System.exit(0));
-		closebutton.setTranslateX(-20);
-		closebutton.setTranslateY(20);
-		StackPane.setAlignment(closebutton, Pos.TOP_RIGHT);
+        Button closebutton = new Button();
+        closebutton.getStyleClass().add("closebutton");
+        closebutton.setOnAction(event -> System.exit(0));
+        closebutton.setTranslateX(-20);
+        closebutton.setTranslateY(20);
+        StackPane.setAlignment(closebutton, Pos.TOP_RIGHT);
 
-		this.getChildren().addAll(hbox, closebutton);
-		this.setMaxHeight(85);
-		StackPane.setAlignment(this, Pos.TOP_CENTER);
-		initializeEvents();
-	}
+        this.getChildren().addAll(hbox, closebutton);
+        this.setMaxHeight(85);
+        StackPane.setAlignment(this, Pos.TOP_CENTER);
+        initializeEvents();
+    }
 
-	private void initializeEvents() {
-		this.setOnMousePressed(new EventHandler<MouseEvent>() {
-			@Override
-			public void handle(MouseEvent event) {
-				xOffset = event.getSceneX();
-				yOffset = event.getSceneY();
-			}
-		});
-		this.setOnMouseDragged(new EventHandler<MouseEvent>() {
-			@Override
-			public void handle(MouseEvent event) {
-				Launcher.getStage().setX(event.getScreenX() - xOffset);
-				Launcher.getStage().setY(event.getScreenY() - yOffset);
-			}
-		});
-	}
+    private void initializeEvents() {
+        this.setOnMousePressed(event -> {
+            xOffset = event.getSceneX();
+            yOffset = event.getSceneY();
+        });
 
-	public static void setVisibility(boolean isVisible) {
-		for(Button btn : _buttons) {
-			btn.setVisible(isVisible);
-		}
-	}
+        this.setOnMouseDragged(event -> {
+            Launcher.getStage().setX(event.getScreenX() - xOffset);
+            Launcher.getStage().setY(event.getScreenY() - yOffset);
+        });
+    }
+
+    public static void setVisibility(boolean isVisible) {
+        for (Button btn : _buttons) {
+            btn.setVisible(isVisible);
+        }
+    }
 
 }
