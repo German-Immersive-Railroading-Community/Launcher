d6504969171e39f58e17ad549f1892a0f6164b64
Shirosaka <shiro@shirosaka.dev>
2026-02-05 01:31:49 +0100

    remove assert and update javadoc

--- src/main/java/com/troblecodings/launcher/Main.java
+++ src/main/java/com/troblecodings/launcher/Main.java
@@ -4,12 +4,14 @@ import com.troblecodings.launcher.util.FileUtil;
 import com.troblecodings.launcher.util.LauncherPaths;
 import javafx.application.Application;
 
+import java.io.IOException;
+
 public final class Main {
     private Main() {
     }
 
-    public static void main(String[] args) {
-        assert LauncherPaths.init();
+    public static void main(String[] args) throws IOException {
+        LauncherPaths.init();
 
         // This needs to happen before any loggers have the chance to be configured.
         System.setProperty("app.root", FileUtil.SETTINGS.baseDir);
